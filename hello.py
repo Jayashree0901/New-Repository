ec61a30ed5d4fcbbec0ea5a4d5b4ddb05223652e
Jayashree <jayashreearunan01@gmail.com>
2024-07-03 19:45:24 +0530

    new changes made

--- hello.py
+++ hello.py
@@ -1,6 +1,17 @@
-print('hello')
+# print('hello')
 
-from collections import deque
-q = deque([5,7,3,9])
-q.popleft()
-print(q)
+# from collections import deque
+# q = deque([5,7,3,9])
+# q.popleft()
+# print(q)
+
+matrix = [
+    [1, 2, 3, 4],
+    [5, 6, 7, 8],
+    [9, 10, 11, 12],
+]
+
+# f = [[row[i] for row in matrix] for i in range(4)]
+
+# print(f)
+print(list(zip(matrix)))
