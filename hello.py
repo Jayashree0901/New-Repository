69f6faaccf262b18a7f4693e46a5f053ac7b43ed
Jayashree Arunan <jayashree.arunan@indigochart.com>
2024-07-09 19:14:31 +0530

    push practice

--- hello.py
+++ hello.py
@@ -5,13 +5,16 @@
 # q.popleft()
 # print(q)
 
-matrix = [
-    [1, 2, 3, 4],
-    [5, 6, 7, 8],
-    [9, 10, 11, 12],
-]
+# matrix = [
+#     [1, 2, 3, 4],
+#     [5, 6, 7, 8],
+#     [9, 10, 11, 12],
+# ]
 
 # f = [[row[i] for row in matrix] for i in range(4)]
 
 # print(f)
-print(list(zip(*matrix)))
+# print(list(zip(*matrix)))
+
+s = '[1,2,3,4]'
+print(eval(s))
